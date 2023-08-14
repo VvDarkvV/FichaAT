0e788a26142036bbeeeb33a6c44df1435acd400a
VvDarkvV <103892355+VvDarkvV@users.noreply.github.com>
2023-08-14 11:48:52 -0300

    1

--- js/script.js
+++ js/script.js
@@ -4,8 +4,23 @@ if (document.getElementById('my-work-link')) {
   })
 }
 
-function valueCheck() {
-  let total = document.querySelectorAll('input[type="checkbox"]:checked').length;
+var atrib = document.querySelectorAll('class="atributo"');
+
+function idCompare(element){
   
-  document.getElementById("forca-valor").innerHTML = total;
+}
+
+function valueCheck() {
+  let total = document.querySelectorAll('input[type="checkbox"]:checked');
+  atrib.forEach((element) =>{
+      var count = 0;
+      for(i = 0; i < total.length; i++){
+      if(total[i].id = element.id){
+        count++;
+      }
+      var valID = element.id + "-valor"
+      document.getElementById(valID).innerHTML = count;
+      }
+  });
+  
 }
