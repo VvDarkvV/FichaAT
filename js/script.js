3f48969ebf1950de556126f0058e403dbe5e27a1
VvDarkvV <103892355+VvDarkvV@users.noreply.github.com>
2023-08-14 12:21:11 -0300

    1

--- js/script.js
+++ js/script.js
@@ -9,14 +9,16 @@ var atrib = document.querySelectorAll('atributo');
 function valueCheck() {
   let total = document.querySelectorAll('input[type="checkbox"]:checked');
   atrib.forEach((element) =>{
-      var count = 0;
-      for(i = 0; i < total.length; i++){
-      if(total[i].id = element.id){
-        count++;
-      }
-      var valID = element.id + "-valor";
-      document.getElementById(valID).innerHTML = count;
-      }
+    console(element.id);
+    var count = 0;
+    for(i = 0; i < total.length; i++){
+    if(total[i].id = element.id){
+      count++;
+    }
+    var valID = element.id + "-valor";
+    console(valID);
+    document.getElementById(valID).innerHTML = count;
+    }
   });
   
 }
